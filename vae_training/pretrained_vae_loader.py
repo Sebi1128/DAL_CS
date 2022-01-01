dbcb1817458d04a8b30b38d6e2ffdb458b04ef7d
AronDiSc <schmied.aron@gmail.com>
2022-01-01 17:14:52 +0100

    Add loader for pretrained models

---
+++ vae_training/pretrained_vae_loader.py
@@ -0,0 +1,23 @@
+"""loads pretrained models from polybox"""
+import requests
+import zipfile
+import io
+import os
+
+path = 'pretrained_models'
+
+links = [
+    r'https://polybox.ethz.ch/index.php/s/qDeyUawrrICvKxJ/download',
+    r'https://polybox.ethz.ch/index.php/s/G15Y7HXicHv0sYq/download',
+    r'https://polybox.ethz.ch/index.php/s/oU3svRBRW6TWItZ/download',
+]
+
+if not os.path.exists(path):
+    os.makedirs(path)
+
+for link in links:
+    print("load ", link)
+    r = requests.get(link)
+    print("unpack ", link)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(path)
